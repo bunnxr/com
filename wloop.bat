76828756c2d6545ed1d85753d2667b691ef8c91f
bunnxr <ibunnxr@gmail.com>
2025-01-12 22:58:41 +0530

    modified:   wloop.bat

--- wloop.bat
+++ wloop.bat
@@ -1,3 +1,3 @@
-Invoke-WebRequest -OutFile C:\charlie\wloop_1080p.ahk -Uri https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop_1080p.ahk
-Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27
-start C:\charlie\Autohotkey.exe C:\charlie\wloop_1080p.ahk
+powershell -command Invoke-WebRequest -OutFile C:\charlie\wloop_1080p.ahk -Uri https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop_1080p.ahk
+powershell -command Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27
+powershell -command start C:\charlie\Autohotkey.exe C:\charlie\wloop_1080p.ahk
