3022ed09af9612f9635c32d8ea8957f546978a8c
bunnxr <ibunnxr@gmail.com>
2025-01-13 13:51:35 +0530

    modified:   wloop.bat 	new file:   wloop_install.ahk

--- wloop.bat
+++ wloop.bat
@@ -1,3 +1,1 @@
-powershell -command Invoke-WebRequest -OutFile C:\charlie\wloop_1080p.ahk -Uri https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop_1080p.ahk
-powershell -command Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27
-powershell -command start C:\charlie\Autohotkey.exe C:\charlie\wloop_1080p.ahk
+powershell -command Invoke-WebRequest -OutFile C:\charlie\temp\wloop_install.exe -Uri 
