3cdce9a4ae1cc961b91ab891f556bc023f5dd9c7
bunnxr <ibunnxr@gmail.com>
2025-01-16 16:42:03 +0530

    new file:   charlieinstall.bat 	deleted:    update.bat 	renamed:    wloop.txt -> wloop.ps1

--- update.bat
+++
@@ -1,5 +0,0 @@
-@echo on
-cd C:\charlie
-curl https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop_1080p.ahk --output C:\charlie\wloop_1080p.ahk
-powershell -command Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27
-start C:\charlie\Autohotkey.exe C:\charlie\wloop_1080p.ahk
