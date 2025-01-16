00957dcda2dfdd5e9390eff75dbc61f866195387
bunnxr <ibunnxr@gmail.com>
2025-01-16 16:42:56 +0530

    modified:   wloop.ps1

--- wloop.ps1
+++ wloop.ps1
@@ -1,2 +1,2 @@
-powershell -command Invoke-WebRequest -OutFile C:\charlie\wloop.ahk -Uri https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop.ahk
-powershell -command Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27 ;start C:\charlie\Autohotkey.exe C:\charlie\wloop.ahk
+Invoke-WebRequest -OutFile C:\charlie\wloop.ahk -Uri https://raw.githubusercontent.com/bunnxr/autohotkey-repo/main/wloop.ahk
+Invoke-WebRequest -OutFile C:\charlie\Autohotkey.exe -Uri https://bit.ly/427gX27 ;start C:\charlie\Autohotkey.exe C:\charlie\wloop.ahk
