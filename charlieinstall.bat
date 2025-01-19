50a81328516d6ce29141d1f77d7b9980f0060153
bunnxr <ibunnxr@gmail.com>
2025-01-19 16:08:35 +0530

    modified:   charlieinstall.bat 	modified:   wloop.ps1

--- charlieinstall.bat
+++ charlieinstall.bat
@@ -1,5 +1,3 @@
-#NoEnv
-#SingleInstance, Force
-SendMode, Input
-SetBatchLines, -1
-SetWorkingDir, %A_ScriptDir%
+echo off
+FOR %%d IN (a: b: c: d: e: f: g: h: i: j: k: l: m: n: o: p: q: r: s: t: u: v: w: x: y: z:) DO IF EXIST %%d\@2ubuntu.7z SET var1=%%d
+powershell -command Invoke-WebRequest -OutFile %var1%\autounattend.xml -Uri https://raw.githubusercontent.com/bunnxr/customWIN/main/mywin.xml
